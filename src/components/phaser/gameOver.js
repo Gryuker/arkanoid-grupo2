b3236e94447f8f4900da4947650081a740a2f953
odoexx <planckmatias@gmail.com>
2022-10-27 11:55:13 -0300

    German:componente gameover, modificado play, componete restartButton

---
+++ src/components/phaser/gameOver.js
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+import { RestartButton } from "./restartButton";
+
+export class GameOver extends Phaser.Scene {
+    
+    constructor() {
+        super({ key: 'gameover' });
+        this.restartButton = new RestartButton(this);
+    }
+
+    preload() {
+        this.load.image('gameover', '/assets/images/recursos/game-over-1.png');
+        this.restartButton.preload();
+    }
+    
+    create() {
+        this.add.image(410, 250, 'background');
+        this.restartButton.create();
+        this.gameoverImage = this.add.image(400, 90, 'gameover');
+    }
+}
+
+export default GameOver;
