54d1fa9ba6b25f9d9d1a1c3a726225d775b10484
EruLuka <EruLukaRuka@gmail.com>
2022-10-28 04:02:31 -0300

    Lucas: Commit Final

--- src/components/phaser/levelButtons.js
+++ src/components/phaser/levelButtons.js
@@ -3,16 +3,23 @@ import Phaser from "phaser";
 export class LevelButtons {
   constructor(scene) {
     this.relatedScene = scene;
+    this.levelText=null;
   }
 
   preload() {
-    this.relatedScene.load.spritesheet('button1', '/assets/images/arkanoid/level1Btn.png', { frameWidth: 195, frameHeight: 49 });
-    this.relatedScene.load.spritesheet('button2', '/assets/images/arkanoid/level2Btn.png', { frameWidth: 195, frameHeight: 49 });
+    this.relatedScene.load.image('button1', '/assets/images/arkanoid/level1Btn.png');
+    this.relatedScene.load.image('button2', '/assets/images/arkanoid/level2Btn.png');
   }
 
   create() {
-    this.level1Button = this.relatedScene.add.sprite(400, 230, 'button1').setInteractive();
-    this.level2Button = this.relatedScene.add.sprite(400, 330, 'button2').setInteractive();
+    this.levelText = this.relatedScene.add.text(this.relatedScene.physics.world.bounds.width/2, this.relatedScene.physics.world.bounds.height/4, "SELECCIÓN DE NIVEL", {
+      fontSize: "60px",
+      fill: "#fff",
+      fontFamily: "verdana, arial, sans-serif",
+    });
+    this.levelText.setOrigin(0.5);
+    this.level1Button = this.relatedScene.add.sprite(250, 330, 'button1').setInteractive().setScale(0.4);
+    this.level2Button = this.relatedScene.add.sprite(550, 330, 'button2').setInteractive().setScale(0.4);
 
     this.level1Button.on('pointerdown', () => {
         this.relatedScene.scene.start('Play', 1);
